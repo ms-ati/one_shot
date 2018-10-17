6146a30ba6a78b7e499322fac3470f56a11d312f
Marc Siegel <marc@usainnov.com>
2018-10-17 15:04:24 -0400

    Start to add specs

--- spec/one_shot/array_like_enumerator_spec.rb
+++ spec/one_shot/array_like_enumerator_spec.rb
@@ -1,13 +1,17 @@
 require_relative "enumerator_decorator_shared_examples"
 
 RSpec.describe OneShot::ArrayLikeEnumerator do
-  subject { instance }
-  let(:instance) { described_class.new(enumerator) }
+  subject(:instance) { described_class.new(enumerator) }
   let(:enumerator) { [].lazy }
 
   describe "#[]" do
-    subject { instance[index] }
+    subject { super()[index] }
     let(:enumerator) { (0..5).lazy }
+
+    context "with negative index" do
+      let(:index) { -1 }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
   end
 
   it_behaves_like "an_enumerator_decorator"
